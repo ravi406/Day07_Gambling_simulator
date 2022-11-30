ea28720ebce5c63bd05fd729150b4d2fff426c5d
ravi406 <ravikamble406@gmail.com>
2022-11-30 22:24:18 +0530

    Added: greeting message 'welcome to Gambling simulator program'

---
+++ src/com/day7/gambling/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.day7.gambling;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator Program");
+    }
+}
