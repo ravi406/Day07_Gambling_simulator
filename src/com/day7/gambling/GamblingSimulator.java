79144b37f8e8e2c8dc7dd6cce1e65004656d0db0
ravi406 <ravikamble406@gmail.com>
2022-11-30 22:50:22 +0530

    added : initial stake amount and amount of bet for every game

--- src/com/day7/gambling/GamblingSimulator.java
+++ src/com/day7/gambling/GamblingSimulator.java
@@ -1,7 +1,11 @@
 package com.day7.gambling;
 
 public class GamblingSimulator {
+    static int amount0fStake = 100;
+    static int betStake=1;
     public static void main(String[] args) {
-        System.out.println("Welcome to Gambling Simulator Program");
+        System.out.println("Welcome to Gambling Simulator Program :");
+        System.out.println("Initaial Amount of Stake :"+amount0fStake);
+        System.out.println("Amount of Bet stake is :"+betStake);
     }
 }
