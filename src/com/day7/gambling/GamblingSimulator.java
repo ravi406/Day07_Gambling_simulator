e4c793e3e686137e99a4971b3c7700271e0c7b4a
ravi406 <ravikamble406@gmail.com>
2022-12-01 10:27:54 +0530

    Added : starting bet stake and minimum bet stake values

--- src/com/day7/gambling/GamblingSimulator.java
+++ src/com/day7/gambling/GamblingSimulator.java
@@ -1,11 +1,12 @@
 package com.day7.gambling;
 
 public class GamblingSimulator {
-    static int amount0fStake = 100;
-    static int betStake=1;
+    public static final int STARTING_BET_STAKE = 100;
+    public static final int MINIMUM_BET_PER_STAKE = 1;
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program :");
-        System.out.println("Initaial Amount of Stake :"+amount0fStake);
-        System.out.println("Amount of Bet stake is :"+betStake);
+        System.out.println("Initaial Amount of Stake :"+STARTING_BET_STAKE);
+        System.out.println("Amount of Bet stake is :"+MINIMUM_BET_PER_STAKE);
     }
 }
+
