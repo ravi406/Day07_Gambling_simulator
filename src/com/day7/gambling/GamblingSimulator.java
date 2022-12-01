b5ad1d0b53153c1ec1f15c3c3f76ccd8f60c1df6
ravi406 <ravikamble406@gmail.com>
2022-12-01 11:46:32 +0530

    Added : logic  for adding 50% extra margin on win or loose

--- src/com/day7/gambling/GamblingSimulator.java
+++ src/com/day7/gambling/GamblingSimulator.java
@@ -1,7 +1,5 @@
 package com.day7.gambling;
 
-import java.util.Random;
-
 public class GamblingSimulator {
     public static final int STARTING_BET_STAKE = 100;
     public static final int MINIMUM_BET_PER_GAME = 1;
@@ -18,13 +16,30 @@ public class GamblingSimulator {
             System.out.println("The Gambler loses the bet and he has $"+remainStake+ " with him after one bet.");
         }
     }
+
+    /*in this method we are calculating gambler win or loose by giving extra margin 50%  */
+    public void gamblingConditionForGame(){
+        double maxMargin = STARTING_BET_STAKE + (0.5*STARTING_BET_STAKE);
+        double minMargin = STARTING_BET_STAKE - (0.5*STARTING_BET_STAKE);
+        double availAmount = STARTING_BET_STAKE;
+        while ((availAmount < maxMargin) && (availAmount > minMargin)){
+            double randomNum = Math.random();
+            if (randomNum > 0.5){
+                availAmount += MINIMUM_BET_PER_GAME;
+            }else{
+                availAmount -= MINIMUM_BET_PER_GAME;
+            }
+        }
+        System.out.println("The gambler resigns for the day after he has $"
+                + availAmount + " with him.");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program :");
-        System.out.println("Initaial Amount of Stake :"+STARTING_BET_STAKE);
+        System.out.println("Initial Amount of Stake :"+STARTING_BET_STAKE);
         System.out.println("Amount of Bet stake is :"+MINIMUM_BET_PER_GAME);
         GamblingSimulator gamblingSimulator = new GamblingSimulator();
         gamblingSimulator.gamblingPerDay();
-
+        gamblingSimulator.gamblingConditionForGame();
     }
 }
 
