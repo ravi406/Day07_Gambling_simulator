a0b9569029200e1e85fca7c63bb4e05d7957c148
ravi406 <ravikamble406@gmail.com>
2022-12-01 11:06:41 +0530

    Added: logic for Gambler makes  or loose  in a game

--- src/com/day7/gambling/GamblingSimulator.java
+++ src/com/day7/gambling/GamblingSimulator.java
@@ -1,12 +1,30 @@
 package com.day7.gambling;
 
+import java.util.Random;
+
 public class GamblingSimulator {
     public static final int STARTING_BET_STAKE = 100;
-    public static final int MINIMUM_BET_PER_STAKE = 1;
+    public static final int MINIMUM_BET_PER_GAME = 1;
+
+    /* in this method Gambler make $1 bet to win or loose $1 */
+    public void gamblingPerDay(){
+        double randomNumber = Math.random();
+        int remainStake = STARTING_BET_STAKE;
+        if (randomNumber > 0.5){
+            remainStake += MINIMUM_BET_PER_GAME;
+            System.out.println("The Gambler wins the bet and he has $"+remainStake+ " with him after one bet.");
+        }else{
+            remainStake -= MINIMUM_BET_PER_GAME;
+            System.out.println("The Gambler loses the bet and he has $"+remainStake+ " with him after one bet.");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program :");
         System.out.println("Initaial Amount of Stake :"+STARTING_BET_STAKE);
-        System.out.println("Amount of Bet stake is :"+MINIMUM_BET_PER_STAKE);
+        System.out.println("Amount of Bet stake is :"+MINIMUM_BET_PER_GAME);
+        GamblingSimulator gamblingSimulator = new GamblingSimulator();
+        gamblingSimulator.gamblingPerDay();
+
     }
 }
 
